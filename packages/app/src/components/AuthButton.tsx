107470983788a988db738ea7b9ec93d3156aad76
Nick Nance <nance.nick@gmail.com>
2019-10-13 07:57:28 -0700

    externalize auth button style

--- packages/app/src/components/AuthButton.tsx
+++ packages/app/src/components/AuthButton.tsx
@@ -1,41 +1,24 @@
 import React from "react";
 import { AppContext } from "./AppContext";
-import { Button, makeStyles, useTheme } from "@material-ui/core";
-import { withRouter } from "react-router-dom";
+import { Button } from "@material-ui/core";
+import { ButtonProps } from "@material-ui/core/Button";
 
-const useStyles = makeStyles(theme => ({
-  link: {
-    margin: theme.spacing(1, 1.5)
-  }
-}));
-
-const AuthButton = () => {
-  const classes = useStyles(useTheme());
-
+const AuthButton = (props: ButtonProps) => {
   const {
     props: { isAuthenticated },
     setAuthenticated
   } = React.useContext(AppContext);
 
   return !isAuthenticated ? (
-    <Button
-      color="primary"
-      variant="outlined"
-      className={classes.link}
-      onClick={() => setAuthenticated(true)}
-    >
+    <Button {...props} onClick={() => setAuthenticated(true)}>
       Sign In
     </Button>
   ) : (
-    <Button
-      color="primary"
-      variant="outlined"
-      className={classes.link}
-      onClick={() => setAuthenticated(false)}
-    >
+    <Button {...props} onClick={() => setAuthenticated(false)}>
       Sign Out
     </Button>
   );
 };
+AuthButton.muiName = "Button";
 
-export default withRouter(AuthButton);
+export default AuthButton;
